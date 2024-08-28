e062aa11f79ad6ee530073b49004c8e0548ba5a9
Ankita Srivastava <ankitasrivastava1009@.com>
2024-08-29 01:18:04 +0530

    ankita-address

--- src/components/AddressListData.js
+++ src/components/AddressListData.js
@@ -27,38 +27,40 @@ const AddressListData = () => {
         <>
 
             <div className="container">
-                <h5>Saved Addresses</h5>
-                <div className="row address-section">
-
+               
+                <div className="row">
+    
                     {/* <div className="col-lg-12">
 
                         <Link to="/address" class="site-btn" >Add a new address</Link>
 
                     </div> */}
 
+                   <div className="col-lg-12 container">
+                    <h5>Saved Addresses</h5>
+                    <div className='row card-deck '>
                     {
-                        addresses.map((address, index) => (
-                            <>
-                                <div className="col-lg-6">
-                                    <div class="services__item bg-secondary bg-opacity-10 mb-3 seller-info">
-                                        {/* <i class="fa fa-car"></i> */}
-                                        <h4 className="heading-custom-font-1">Address - {index + 1}</h4>
-                                        <p>Name - {address.name}</p>
-                                        <p>Address - {address.line1}</p>
-                                        <p>City - {address.city}, {address.state}, {address.country}, {address.pin},</p>
-                                        <p>Landmark - {address.landmark}</p>
-                                        <p>Contact - {address.contact}&nbsp;&nbsp; {address.alternatecontact}</p>
-                                    </div>
+                        addresses.map((item) => (
+                            
+                                <div class="services__item bg-secondary bg-opacity-10 mb-3 rounded-4 p-4  card" style={{maxWidth:'450px', marginLeft:'25px'}}>
+                                    <i class="fa fa-car"></i>
+                                    <h5>Name - {item.name}</h5>
+                                    <p>{item.line1}&nbsp;{item.line2}&nbsp;{item.line3}</p>
+                                    <p>{item.city}&nbsp;{item.state}&nbsp;{item.country}</p>
+                                    <p>{item.pin}</p>
+                                    <p>Mobile - {item.contact}&nbsp;{item.alternatecontact}</p>
+                                    <p>Landmark - {item.landmark}</p>
                                 </div>
-                            </>
+                           
                         ))
                     }
-
+                    </div>
 
                 </div>
             </div>
+        </div>
 
-        </>
+    </>
     )
 }
 
