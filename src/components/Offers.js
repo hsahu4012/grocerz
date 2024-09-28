71885f5a085fa7741d50dac48fab11f06e5468a1
lovrezchaudhary <lovrezchaudhary@gmail.com>
2024-09-28 17:27:14 +0530

    update containers

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -58,45 +58,49 @@ const Offers = () => {
           teamMembers.map(member => (
             <div className='d-flex justify-content-center' key={member.id}>
               <div
-                className='d-flex flex-column align-items-center p-4'
+                className='d-flex flex-row align-items-center p-4'
                 style={{
-                  backgroundImage: 'url("../src/assets/images/discount.png")',
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  borderRadius: '8px',
-                  maxWidth: '400px',
-                  textAlign: 'center',
+                  backgroundColor: 'rgba(189, 130, 238, 0.4)',
+                  width: '100%',
+                  maxWidth: '1200px', 
+                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                  display: 'flex',
                 }}
               >
-                <div className='text-center mb-3'>
+                <div className='text-center mr-4'>
                   {member.image && (
                     <img
                       src={`${process.env.REACT_APP_API_URL}${member.image}`}
                       alt={member.discountname}
-                      width='120'
+                      width='150'
                       className='img-fluid'
                     />
                   )}
                 </div>
-                <h2>{member.discountname}</h2>
-                <p>
-                  PROMOCODE: {member.discountid}
-                  <br />
-                  {member.amount > 0 && `Fixed Discount: ₹${member.amount}`}
-                  <br />
-                  {member.percentage > 0 &&
-                    `Percentage Discount: ${member.percentage}%`}
-                  <br />
-                  Valid Till: {member.enddate.substring(8, 10)}/
-                  {member.enddate.substring(5, 7)}/
-                  {member.enddate.substring(0, 4)}
-                </p>
-                <a
-                  href='/product-sidebar.html'
-                  className='btn btn-warning mt-4'
-                >
-                  Shop Now
-                </a>
+                <div>
+                  <h2>{member.discountname}</h2>
+                  <p>
+                    PROMOCODE: {member.discountid}
+                    <br />
+                    {member.amount > 0 && `Fixed Discount: ₹${member.amount}`}
+                    <br />
+                    {member.percentage > 0 &&
+                      `Percentage Discount: ${member.percentage}%`}
+                    <br />
+                    Valid Till: {member.enddate.substring(8, 10)}/
+                    {member.enddate.substring(5, 7)}/
+                    {member.enddate.substring(0, 4)}
+                  </p>
+                  {/* <div className='product-cart-btn mt-4'>
+                    <button
+                      onClick={() => handleViewDetails(member)}
+                      className='product-btn'
+                      type='button'
+                    >
+                      View Details
+                    </button>
+                  </div> */}
+                </div>
               </div>
             </div>
           ))}
@@ -114,10 +118,10 @@ const Offers = () => {
                   style={{
                     backgroundColor:
                       index === 0
-                        ? 'rgba(240, 146, 159, 0.28)'
-                        : 'rgba(104, 199, 236, 0.28)', // Different colors for each container
+                        ? 'rgba(240, 146, 159, 0.4)'
+                        : 'rgba(104, 199, 236, 0.4)', // Different colors for each container
                     border: '1px solid rgba(0, 0, 0, 0.1)', // Light border for contrast
-                    borderRadius: '8px',
+                    borderRadius: '4px',
                     textAlign: 'center',
                     width: '100%',
                     minHeight: '300px', // Ensures both containers have the same height
@@ -134,7 +138,7 @@ const Offers = () => {
                       />
                     )}
                   </div>
-                  <h2>{member.discountname}</h2>
+                  <h3>{member.discountname}</h3>
                   <p>
                     PROMOCODE: {member.discountid}
                     <br />
@@ -147,18 +151,83 @@ const Offers = () => {
                     {member.enddate.substring(5, 7)}/
                     {member.enddate.substring(0, 4)}
                   </p>
-                  <div className='product-cart-btn'>
+                  {/* <div className='product-cart-btn mt-4'>
                     <button
                       onClick={() => handleViewDetails(member)}
-                      className='product-btn btn btn-warning mt-4' // Applied Bootstrap styling for consistency
+                      className='product-btn'
                       type='button'
                     >
                       View Details
                     </button>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             ))}
+          </div>
+        )}
+
+        {/* Render design if three or more discounts are available */}
+        {teamMembers.length >= 3 && (
+          <div className='container'>
+            <div className='row justify-content-center'>
+              {teamMembers.map((member, index) => (
+                <div
+                  className='col-md-4 d-flex justify-content-center mb-5 mt-5'
+                  key={member.id}
+                >
+                  <div
+                    className='d-flex flex-column align-items-center p-4'
+                    style={{
+                      backgroundColor:
+                        index % 3 === 0
+                          ? 'rgba(240, 146, 159, 0.28)'
+                          : index % 3 === 1
+                            ? 'rgba(246, 240, 56, 0.4)'
+                            : 'rgba(255, 193, 102, 0.28)', // Different colors for each set of three containers
+                      border: '1px solid rgba(0, 0, 0, 0.1)', // Light border for contrast
+                      borderRadius: '8px',
+                      textAlign: 'center',
+                      width: '100%', // Full width within the column
+                      minHeight: '300px', // Ensures containers have the same height
+                      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // Light shadow for depth
+                    }}
+                  >
+                    <div className='text-center mb-3'>
+                      {member.image && (
+                        <img
+                          src={`${process.env.REACT_APP_API_URL}${member.image}`}
+                          alt={member.discountname}
+                          width='120'
+                          className='img-fluid'
+                        />
+                      )}
+                    </div>
+                    <h3>{member.discountname}</h3>
+                    <p>
+                      PROMOCODE: {member.discountid}
+                      <br />
+                      {member.amount > 0 && `Fixed Discount: ₹${member.amount}`}
+                      <br />
+                      {member.percentage > 0 &&
+                        `Percentage Discount: ${member.percentage}%`}
+                      <br />
+                      Valid Till: {member.enddate.substring(8, 10)}/
+                      {member.enddate.substring(5, 7)}/
+                      {member.enddate.substring(0, 4)}
+                    </p>
+                    {/* <div className='product-cart-btn mt-4'>
+                    <button
+                      onClick={() => handleViewDetails(member)}
+                      className='product-btn'
+                      type='button'
+                    >
+                      View Details
+                    </button>
+                  </div> */}
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         )}
 
