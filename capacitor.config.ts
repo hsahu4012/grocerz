f87e64919cec84cbb5d5a12f6ac187749717a53c
deepak104080 <deepak104080@gmail.com>
2024-10-23 11:19:44 +0530

    Mobile Icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.grocji.app',
   appName: 'grocji',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      backgroundColor: "#ff0000",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      splashFullScreen: true,
+      splashImmersive: true,
+    }
+  }
 };
 
 export default config;
