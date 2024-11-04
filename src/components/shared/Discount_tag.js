c03ca78c72ed9319d6cc81b09e6494c5243fcc05
PratikTech07 <146507779+PratikTech07@users.noreply.github.com>
2024-11-04 21:10:35 +0530

    Solved -  Dont use inline CSS. Use BootStrap or write in App.css

--- src/components/shared/Discount_tag.js
+++ src/components/shared/Discount_tag.js
@@ -4,21 +4,10 @@ const Discount = ({price , discount}) => {
 
     
     const discountPrecentage = (discount / price ) * 100;
-    const discountTagStyle = {
-        position: 'absolute',
-        left: '5%',
-        backgroundColor: '#34A853',
-        color: 'white',
-        padding: '5px 10px',
-        fontSize: '12px',
-        fontWeight: 'bold',
-        clipPath: 'polygon(0 0, 100% 0, 100% 100%, 83% 89%, 64% 100%, 40% 90%, 18% 100%, 0 90%)',
-        zIndex: 1
-        
-    };
+    
     return(
         <>
-            <div className="dis_tag" style={discountTagStyle} isVisible>
+            <div className="dis_tag"  isVisible>
                 {Math.ceil(discountPrecentage)} %<br/>
                  OFF</div>
         </>
